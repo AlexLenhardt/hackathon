267326a8ecc136cfdd0df8afb7f50a766057a220
EdsonZuchi <102428713+EdsonZuchi@users.noreply.github.com>
2023-04-21 10:05:48 -0300

    created tables

--- src/main/kotlin/com/hackathon/HackathonApplication.kt
+++ src/main/kotlin/com/hackathon/HackathonApplication.kt
@@ -1,5 +1,8 @@
 package com.hackathon
 
+import com.hackathon.called.infrastructure.repository.database.CalledDatabase
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +10,8 @@ import org.springframework.boot.runApplication
 class HackathonApplication
 
 fun main(args: Array<String>) {
+	transaction {
+		SchemaUtils.create(CalledDatabase)
+	}
 	runApplication<HackathonApplication>(*args)
 }
