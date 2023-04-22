856f87d0c76cdbff2ae7fc488dd2ef9d90b525a4
EdsonZuchi <102428713+EdsonZuchi@users.noreply.github.com>
2023-04-22 16:06:48 -0300

    last commit

--- src/main/kotlin/com/hackathon/ticket/infrastructure/repository/database/DescriptionTicket.kt
+++ src/main/kotlin/com/hackathon/ticket/infrastructure/repository/database/DescriptionTicket.kt
@@ -2,6 +2,7 @@ package com.hackathon.ticket.infrastructure.repository.database
 
 import com.hackathon.user.infrastructure.repository.database.UserDatabase
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.date
 import org.jetbrains.exposed.sql.javatime.datetime
 
@@ -10,6 +11,6 @@ object DescriptionTicketDatabase : Table("description_ticket") {
     var ticketUUID = reference("uuid_ticket", TicketDatabase.uuid)
     var situationUUID = reference("uuid_situation", SituationDatabase.uuid)
     var userUUID = reference("uuid_user", UserDatabase.uuid)
-    var create_at = datetime("create_at")
+    var create_at = datetime("create_at").defaultExpression(CurrentDateTime)
     var description = text("description")
 }
